8fca2404ee78ed7e5cb1694a2ac7d23ad189a2f1
glowkeeper <steve.huckle@gmail.com>
2024-12-06 17:25:59 +0000

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import { Canvas } from '@react-three/fiber'
 //import { OrbitControls } from '@react-three/drei'
 
 import { Header, Footer, Model } from './components'
-import { Animations } from './utils'
 
 export const App = () => {
 
   return (
     <>
-      <Header />
+      <Header />      
       <main>
-        <p>
-          {Animations.walking.title}: <strong>{Animations.walking.key}</strong>, {Animations.idle.title}: <strong>{Animations.idle.key}</strong>, {Animations.standingJump.title}: <strong>{Animations.standingJump.key}</strong>, {Animations.running.title}: <strong>{Animations.running.key}</strong>
-        </p> 
         <div
           id="AnimationCanvas"
         >
           <Canvas camera={{ fov: 50, zoom: 1, near: 1, far: 1000, position: [0, 0, 5], rotation: [0, 0, 0]}}>
             <Suspense fallback={null}>
-              <ambientLight intensity={0.5} />
+              <ambientLight intensity={0.9} />
               <directionalLight />
               {/* <OrbitControls makeDefault/> */}
               <Model />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
+import { Animations } from '../utils'
+
 export const Header = () => {
 
   return (
     <header>
+        <ul>
+            <li>{Animations.walking.title}: <strong>{Animations.walking.key}</strong></li>
+            <li>{Animations.idle.title}: <strong>{Animations.idle.key}</strong></li>
+            <li>{Animations.standingJump.title}: <strong>{Animations.standingJump.key}</strong></li>
+            <li>{Animations.running.title}: <strong>{Animations.running.key}</strong></li> 
+        </ul>
         <h1>Animated Dr Huckle</h1>
+        <div>&nbsp;</div>
     </header>
   );
 }
